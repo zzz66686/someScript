48e33ac009cb3dc6c398e1e1a9a7a8671eaaa092
zzz66686 <zzz66686@sina.com>
2023-02-21 12:02:07 +0800

    Update fakeudp.py

--- fakeudp.py
+++ fakeudp.py
@@ -1,6 +1,6 @@
 from scapy.all import *
 
-pkt = Ether(src='A4:42:3B:70:FE:59', dst='FC:34:97:5C:0C:F0')/IP(src='1', dst="127.0.0.1")/UDP(sport=13338, dport=61689)/"\x00AAAAAAAAAAAAAAAAAAAA"
+pkt = Ether(src='A4:42:3B:70:FE:59', dst='FC:34:97:5C:0C:F0')/IP(src='192.168.1.250', dst="192.168.1.1")/UDP(sport=13338, dport=5351)/"\x00AAAAAAAAAAAAAAAAAAAA"
 
 while(True):
     sendp(pkt)
