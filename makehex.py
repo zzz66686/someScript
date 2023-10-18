e6ca2288fc154e128bb0879c7a7e0ff6e2cc9692
zzz66686 <zzz66686@sina.com>
2023-10-18 13:33:23 +0800

    Update makehex.py

--- makehex.py
+++ makehex.py
@@ -1,9 +1,23 @@
 import binascii
 
-with open('rawdata.bin', 'r') as f:
+
+with open('rawdata.txt', 'rb') as f:
     content = f.read()
 
 writeData = binascii.a2b_hex(content)
+
+
+with open('rawdata.bin', 'wb') as f:
+    f.write(writeData)
+
+
+
+
+
+with open('rawdata.bin', 'rb') as f:
+    content = f.read()
+
+writeData = binascii.b2a_hex(content)
 
 with open('rawdata.txt', 'wb') as f:
     f.write(writeData)
